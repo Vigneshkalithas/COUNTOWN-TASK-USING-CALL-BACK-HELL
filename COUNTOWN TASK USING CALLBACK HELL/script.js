f57c93245b11e53b858cc1270e5b0d1902758f50
vigneshkalithas <96053261+Vigneshkalithas@users.noreply.github.com>
2022-04-02 22:52:01 +0530

    Add files via upload

---
+++ COUNTOWN TASK USING CALLBACK HELL/script.js
@@ -0,0 +1,54 @@
+var i = 10;
+   setTimeout(function(){
+       document.getElementById('new').innerHTML = i;
+           setTimeout(function(){
+               i--;
+               document.getElementById('new').innerHTML = i;
+                    setTimeout(function(){
+                        i--;
+                         document.getElementById('new').innerHTML = i;
+                           setTimeout(function(){
+                             i--;
+                               document.getElementById('new').innerHTML = i;
+                                  setTimeout(function(){
+                                     i--;
+                                     document.getElementById('new').innerHTML = i;
+                                         setTimeout(function(){
+                                            i--;
+                                              document.getElementById('new').innerHTML = i;
+                                                setTimeout(function(){
+                                                 i--;
+                                                   document.getElementById('new').innerHTML = i;
+                                                     setTimeout(function(){
+                                                        i--;
+                                                      document.getElementById('new').innerHTML = i;
+                                                        setTimeout(function(){
+                                                            i--;
+                                                          document.getElementById('new').innerHTML = i;
+                                                             setTimeout(function(){
+                                                               i--;
+                                                                 document.getElementById('new').innerHTML = i;
+                                                                     setTimeout(function(){
+                                                                          i--;
+                                                                        document.getElementById('new').innerHTML = i;
+                                                                          setTimeout(function(){
+                                                                              i--;
+                                                                              document.getElementById('new').innerHTML = "Happy Independence Day";
+                                                                                  setTimeout(function(){
+                                                                                      i--;
+                                                                          },1200)
+                                                                    },1200)
+                                                             },1200)
+                                                         },1200)
+                                                       },1200)
+                                                  },1200)
+                                              },1200)
+
+                                         },1200)
+                         
+                                     },1200)
+                                 },1200)
+                             },1200)
+
+                           },1200)
+                      },1200)
